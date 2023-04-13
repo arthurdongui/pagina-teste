15c10a55136411cc63851244069f908bf42f827b
arthurdongui <129235661+arthurdongui@users.noreply.github.com>
2023-04-13 18:43:33 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,30 @@
+console.log("Bem vindos a Micro escritor!") 
+
+//Apresentação
+const person ={
+    name: 'Arthur Dongui',
+    age: '19',
+    weight: '62.5',
+    ISadmin: true
+}
+console.log(`${person.name} tem ${person.age} anos`)
+//Apresentação
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
